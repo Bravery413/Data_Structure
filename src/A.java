b5ef38a05aad99a16e6ed9f136a443a9ed573247
py <123456@qq.com>
2019-07-04 17:06:53 +0800

    this.

--- src/A.java
+++ src/A.java
@@ -1,4 +1,9 @@
 public class A {
+// this关键字主要有三个应用：
+//     (1)this调用本类中的属性，也就是类中的成员变量；
+//     (2)this调用本类中的其他方法；
+//     (3)this调用本类中的其他构造方法，调用时要放在构造方法的首行。
+
     public static int i=1;
     public A(int i){
         int k=this.i;//为10
