2bb61981b76f6ac9cb69c39fffd24dd3d58babd8
py <123456@qq.com>
2019-07-19 08:57:21 +0800

    leetCode*6

---
+++ src/leetCode/Solution104.java
@@ -0,0 +1,48 @@
+package leetCode;
+
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 给定一个二叉树，找出其最大深度。
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ */
+public class Solution104 {
+    //树的遍历
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+            return java.lang.Math.max(left_height, right_height) + 1;
+        }
+    }
+
+    //栈的帮助下将上面的递归转换为迭代。
+    public static int maxDepth1(TreeNode root) {
+        Queue<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+        if (root != null) {
+            stack.add(new Pair(root, 1));
+        }
+
+        int depth = 0;
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> current = stack.poll();
+            root = current.getKey();
+            int current_depth = current.getValue();
+            if (root != null) {
+                depth = Math.max(depth, current_depth);
+                stack.add(new Pair(root.left, current_depth + 1));
+                stack.add(new Pair(root.right, current_depth + 1));
+            }
+        }
+        return depth;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/leetCode/Solution2.java
+++ src/leetCode/Solution2.java
@@ -12,27 +12,52 @@ package leetCode;
 
 
 public class Solution2 {
-    /**
-     * 思路:
-     * 1. 遍历,两个结点相加,*10
-     */
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    //自己的方法,多了个最后的0,不知道如何去
+    public static ListNode addTwoNumbers0(ListNode l1, ListNode l2) {
         ListNode listNode = new ListNode(0);
         ListNode pre = listNode;
+
         while (l1 != null && l2 != null) {
             pre.next = new ListNode(0);
             int value = l1.val + l2.val;
             if (value >= 10) {
-                pre.next.val = value % 10;
-                pre.val++;
+                pre.val += value % 10;
+                pre.next.val += 1;
             } else {
-                pre.next.val = l1.val + l2.val;
+                pre.val += value;
             }
             l1 = l1.next;
             l2 = l2.next;
             pre = pre.next;
         }
-        return listNode.next;
+        return listNode;
+    }
+
+    /**
+     * 思路:
+     * 1. 遍历,两个结点相加,
+     * 2. 用一个遍历存储进位
+     */
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+
     }
 
     /**
