2a75d6de0f45cf4d1ffef0dfc44ab26c4b8fd5cb
py <123456@qq.com>
2019-07-19 17:16:09 +0800

    leetCode*3

--- src/leetCode/Solution11.java
+++ src/leetCode/Solution11.java
@@ -1,5 +1,7 @@
 package leetCode;
 
+import sun.security.util.Length;
+
 /**
  * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
  * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
@@ -7,35 +9,42 @@ package leetCode;
  */
 public class Solution11 {
     //暴力 不解释
-    public static int maxArea(int[] height) {
-        int length=0;
-        int max=0;
-        int temp=0;
-        for (int i=0;i<height.length;i++){
-            for (int j=1;j<height.length;j++){
-                length=j-i;
+    public static int maxArea0(int[] height) {
+        int length = 0;
+        int max = 0;
+        int temp = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = 1; j < height.length; j++) {
+                length = j - i;
                 int min = Math.min(height[i], height[j]);
-                temp=length*Math.min(height[i],height[j]);
-                max=max>temp?max:temp;
+                temp = length * Math.min(height[i], height[j]);
+                max = max > temp ? max : temp;
             }
         }
         return max;
     }
+
     //双指针法
-    public int maxArea0(int[] height) {
-        int maxarea = 0, l = 0, r = height.length - 1;
-        while (l < r) {
-            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
-            if (height[l] < height[r])
-                l++;
-            else
-                r--;
+    public static int maxArea(int[] height) {
+        int left=0;
+        int right=height.length-1;
+        int max=-1;
+        while (left<right){
+            int len=right-left;
+            //短板往前走
+            if (height[left]>height[right]){
+                max=Math.max(max,len*Math.min(height[left],height[right]));
+                right--;
+            }else {
+                max=Math.max(max,len*Math.min(height[left],height[right]));
+                left++;
+            }
         }
-        return maxarea;
+        return max;
     }
 
     public static void main(String[] args) {
-        int[] height=new int[]{8,6,2,5,4,8,3,7};
+        int[] height = new int[]{8, 6, 2, 5, 4, 8, 3, 7};
         System.out.println(maxArea(height));
     }
 }
---
+++ src/leetCode/Solution217.java
@@ -0,0 +1,23 @@
+package leetCode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution217 {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> se = new HashSet<>();
+        for (Integer i : nums) {
+            if (se.contains(i))
+                return true;
+            se.add(i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+
+}
