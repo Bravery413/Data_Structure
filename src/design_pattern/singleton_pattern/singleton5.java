67cf5014686e624c3bc03ae947807819970e3425
py <123456@qq.com>
2019-07-23 11:28:30 +0800

    设计模式:单例模式

---
+++ src/design_pattern/singleton_pattern/singleton5.java
@@ -0,0 +1,16 @@
+package design_pattern.singleton_pattern;
+
+public class singleton5 {
+    //5=0
+//    private static class SingletonHolder {
+//        private static final Singleton5 INSTANCE = new singleton5();
+//    }
+//
+//    private Singleton5() {
+//    }
+//    public static final singleton5 getInstance(){
+//        return singleton5.INSRANCE;
+//    }
+
+
+}
