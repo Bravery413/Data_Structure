ab9a2c70d3baa33cbbd0b82644e3866b4bafcb8e
bravery <2472937751@qq.com>
2019-08-20 22:33:15 +0800

    leetCode 队列栈专题

---
+++ src/leetCode/ArrayAndStack/Solution150.java
@@ -0,0 +1,30 @@
+package leetCode.ArrayAndStack;
+
+import java.util.Stack;
+
+/**
+ * @author: Bravery
+ * @create: 2019-08-20 22:21
+ **/
+
+
+public class Solution150 {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String c:tokens) {
+            if (c.equals("+")){
+                stack.push(stack.pop()+stack.pop());
+            }else if (c.equals("-")){
+                stack.push(-stack.pop()+stack.pop());
+            }else if (c.equals("*")){
+                stack.push(stack.pop()*stack.pop());
+            }else if (c.equals("/")){
+                int num=stack.pop();
+                stack.push(stack.pop()/num);
+            }else {
+                stack.push(Integer.valueOf(c));
+            }
+        }
+        return stack.pop();
+    }
+}
---
+++ src/leetCode/ArrayAndStack/Solution20.java
@@ -0,0 +1,37 @@
+package leetCode.ArrayAndStack;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * @author: Bravery
+ * @create: 2019-08-20 21:16
+ **/
+
+
+public class Solution20 {
+    public Solution20() {
+
+    }
+
+    public boolean isValid(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('}', '{');
+        map.put(']', '[');
+        map.put(')', '(');
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                char topElement = stack.isEmpty() ? '#' : stack.pop();
+                if (topElement != map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
