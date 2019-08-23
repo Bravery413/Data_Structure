746d322c0a98173c3a9b7524517d6e96e80d7fbb
py <123456@qq.com>
2019-08-23 18:04:25 +0800

    LeetCode二分查找章节

---
+++ src/leetCode/binarySearch/Solution704.java
@@ -0,0 +1,15 @@
+package leetCode.binarySearch;
+
+/**
+ * @author bravery
+ * @date 2019/8/23 18:03
+ */
+
+/**
+ * 最简单的二分查找
+ */
+public class Solution704 {
+    public int search(int[] nums, int target) {
+        return 1;
+    }
+}
