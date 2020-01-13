81c1f5c88be2ad94265f5b8d1e13c9f98d95fded
潘勇 <2472937751@qq.com>
2020-01-13 18:26:44 +0800

    #第五题未写完

---
+++ src/offer/Question5.java
@@ -0,0 +1,26 @@
+package offer;
+
+import java.util.Stack;
+
+/**
+ * 两个链表第一个公共节点
+ */
+public class Question5 {
+
+    public static ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+
+        Stack<ListNode> s1 = new Stack<>();
+        Stack<ListNode> s2 = new Stack<>();
+//    TODO 明天继续写
+//        while ()
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        ListNode pHead1 = new ListNode(1);
+
+    }
+
+
+}
