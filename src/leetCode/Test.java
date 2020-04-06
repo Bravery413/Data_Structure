0beca805c4185c8230a901f214a26d90937c350a
pan_yong <2472937751@qq.com>
2020-04-06 23:06:12 +0800

    # 完成LeetCode3题(金典123) 未总结

--- src/leetCode/Test.java
+++ src/leetCode/Test.java
@@ -1,8 +1,18 @@
 package leetCode;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         long start = System.currentTimeMillis();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd");
+        Date date = simpleDateFormat.parse("2016-11-13");
+        String time = simpleDateFormat.format(date);
+        long time1 = date.getTime();
+
+
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
