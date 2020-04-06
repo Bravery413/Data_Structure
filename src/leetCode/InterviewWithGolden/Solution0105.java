d9e9f822cf669002fa988b45892145efb4dde620
pan_yong <2472937751@qq.com>
2020-04-07 00:18:48 +0800

    # 完成LeetCode2题(金典45) 1.是字符数量问题可以考虑位图 2.是字符比较问题可以考虑双指针

--- src/leetCode/InterviewWithGolden/Solution0105.java
+++ src/leetCode/InterviewWithGolden/Solution0105.java
@@ -1,6 +1,9 @@
 package leetCode.InterviewWithGolden;
 
-
+/**
+ * 字符串有三种编辑操作:插入一个字符、删除一个字符或者替换一个字符。
+ * 给定两个字符串，编写一个函数判定它们是否只需要一次(或者零次)编辑。
+ */
 public class Solution0105 {
     public static boolean oneEditAway1(String first, String second) {
         int count = 0;
