7e4f8ee76469b4da76c9b15689209de7b4994f20
pan_yong <2472937751@qq.com>
2020-05-28 22:49:03 +0800

    # 完成LeetCode2题(金典) 1.有序数组创建一棵平衡二叉树:树可利用递归快速解决 2.3步N阶楼梯:1.规律是前三步的和;2.效率更高的动态规划

---
+++ src/leetCode/InterviewWithGolden/Solution0402.java
@@ -0,0 +1,40 @@
+package leetCode.InterviewWithGolden;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个有序整数数组，元素各不相同且按升序排列，
+ * 编写一个算法，创建一棵高度最小的二叉搜索树。
+ */
+public class Solution0402 {
+    /**
+     * 一般树,可以考虑递归
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST1(int[] nums) {
+        if (nums == null || nums.length == 0) return null;
+        int mid = nums.length / 2;
+        TreeNode root = new TreeNode(mid);
+        root.left = sortedArrayToBST(Arrays.copyOfRange(nums, 0, mid));
+        root.right = sortedArrayToBST(Arrays.copyOfRange(nums, mid + 1, nums.length));
+        return root;
+    }
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return treeHelper(nums,0,nums.length-1);
+    }
+
+    private TreeNode treeHelper(int [] nums,int left,int right){
+
+        if(left > right){
+            return null;
+        }
+        int mid = (left + right)/2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = treeHelper(nums,left,mid-1);
+        node.right = treeHelper(nums,mid+1,right);
+        return node;
+    }
+
+
+}
