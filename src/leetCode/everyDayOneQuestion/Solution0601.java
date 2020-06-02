5b991b7f62e575b2cb3ca917477aaa839e122f1b
潘勇 <panyong11@sfmail.sf-express.com>
2020-06-02 08:52:38 +0800

    # LeetCode 打卡 1.6.1 数糖果 2.6.2 1-n的累加

---
+++ src/leetCode/everyDayOneQuestion/Solution0601.java
@@ -0,0 +1,30 @@
+package leetCode.everyDayOneQuestion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution0601 {
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int len = candies.length;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < len; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+        ArrayList<Boolean> res = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            if (candies[i] + extraCandies >= max) {
+                res.add(true);
+            } else {
+                res.add(false);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] qus=new int[]{2,3,5,1,3};
+        System.out.println(kidsWithCandies(qus,3));
+    }
+}
