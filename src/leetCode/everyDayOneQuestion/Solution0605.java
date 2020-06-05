cba57df4c4011ccd3aa5d9b162be042dd1761a83
pan_yong <2472937751@qq.com>
2020-06-05 23:55:04 +0800

    # 打卡LeetCode1题 顺时针打印矩阵:按步骤思维,细心,耐心

---
+++ src/leetCode/everyDayOneQuestion/Solution0605.java
@@ -0,0 +1,24 @@
+package leetCode.everyDayOneQuestion;
+
+public class Solution0605 {
+    public int[] spiralOrder(int[][] matrix) {
+        if (matrix == null||matrix.length==0) return new int[0];
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int top = 0;
+        int botton = matrix.length - 1;
+        int[] res = new int[matrix[0].length * matrix.length];
+        int x = 0;
+        while (true) {
+            for (int i = left; i <= right; i++) res[x++] = matrix[top][i];
+            if (++top > botton) break;
+            for (int i = top; i <= botton; i++) res[x++] = matrix[i][right];
+            if (left > --right) break;
+            for (int i = right; i >= left; i--) res[x++] = matrix[botton][i];
+            if (top > --botton) break;
+            for (int i = botton; i >= top; i--) res[x++] = matrix[i][left];
+            if (++left > right) break;
+        }
+        return res;
+    }
+}
