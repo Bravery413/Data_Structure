81cd375118aa395fcb7432f5d3fd621c9a0063c4
pan_yong <2472937751@qq.com>
2020-06-16 23:31:32 +0800

    # 打卡LeetCode1题 树的序列化和反序列化(完成度不好) 1.数子两位数用"," 2.负数的判断 3.空的判断 有空要细做下

---
+++ src/leetCode/everyDayOneQuestion/june/Solution0616.java
@@ -0,0 +1,18 @@
+package leetCode.everyDayOneQuestion.june;
+
+import leetCode.InterviewWithGolden.TreeNode;
+
+public class Solution0616 {
+//    public class Codec {
+//
+//        // Encodes a tree to a single string.
+//        public String serialize(TreeNode root) {
+//            return "";
+//        }
+//
+//        // Decodes your encoded data to tree.
+//        public TreeNode deserialize(String data) {
+//            return :
+//        }
+//    }
+}
---
+++ src/leetCode/everyDayOneQuestion/june/TreeNode.java
@@ -0,0 +1,13 @@
+package leetCode.everyDayOneQuestion.june;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+
+}
