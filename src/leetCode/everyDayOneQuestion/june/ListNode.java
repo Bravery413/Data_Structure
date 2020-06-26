43012aec7aae24085ed523d70dcca77bb8c2d02c
潘勇 <panyong11@sfmail.sf-express.com>
2020-06-26 11:19:05 +0800

    # 打卡LeetCode3题 1.最接近的三数之和:排序+双指针,考coding能力 2.单词拆分:动态规划,可以再加深下理解(还没理解效率最高的方法) 3.移除重复节点:1哈希2.位图3.双循环枚举,时间换空间

---
+++ src/leetCode/everyDayOneQuestion/june/ListNode.java
@@ -0,0 +1,13 @@
+package leetCode.everyDayOneQuestion.june;
+
+/**
+ * @author 潘勇
+ * @date 2020/6/26 9:16
+ */
+
+
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
