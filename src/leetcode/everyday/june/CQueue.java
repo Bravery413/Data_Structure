b0f79e12429b20a8b107eef1692a14f10fb92614
潘勇 <2472937751@qq.com>
2020-07-12 23:30:45 +0800

    # Leetcode 补卡2题 1.数组中的第K个最大元素 2.两个栈实现队列

---
+++ src/leetcode/everyday/june/CQueue.java
@@ -0,0 +1,62 @@
+package leetcode.everyday.june;
+
+import linearList.myQueue.ArrayQuene;
+
+import java.util.Stack;
+
+/**
+ * @author: Bravery
+ * @create: 2020-07-12 23:01
+ * 剑指 Offer 09. 用两个栈实现队列
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ * 示例 2：
+ *
+ * 输入：
+ * ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]]
+ * 输出：[null,-1,null,null,5,2]
+ * 提示：
+ *
+ * 1 <= values <= 10000
+ * 最多会对 appendTail、deleteHead 进行 10000 次调用
+ **/
+
+
+public class CQueue {
+    Stack<Integer> in;
+    Stack<Integer> out;
+
+    public CQueue() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        in.push(value);
+    }
+
+    public int deleteHead() {
+        if (!out.empty()) {
+            return out.pop();
+        }
+        if (in.size() == 0) {
+            return -1;
+        }
+        while (!in.isEmpty()) {
+            out.push(in.pop());
+        }
+        return out.pop();
+    }
+
+
+
+}
