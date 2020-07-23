5d817bd95aa62cdd090c50788551e5d52a967fee
潘勇 <2472937751@qq.com>
2020-07-23 21:34:07 +0800

    # Leetcode 打卡1题 1.最小路径和,动态规划,创建dp[][]结果集数组

---
+++ src/leetcode/everyday/july/Solution0717.java
@@ -0,0 +1,40 @@
+package leetcode.everyday.july;
+
+import java.util.Arrays;
+
+/**
+ * @author: Bravery
+ * @create: 2020-07-17 23:05
+ **/
+
+
+public class Solution0717 {
+    public static int searchInsert(int[] nums, int target) {
+        return searchInsert(nums, target, 0, nums.length);
+
+    }
+
+    public static int searchInsert(int[] nums, int target, int left, int right) {
+        if (left >= right) {
+           if (nums[left]>target){
+               return left;
+           }else {
+               return left+1;
+           }
+        }
+        int mid = (left + right) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+        return searchInsert(nums, target, left, right);
+    }
+
+    public static void main(String[] args) {
+        int[] nums={1,3,5,6};
+        System.out.println(searchInsert(nums,2));
+    }
+}
