4f7e6c1c03cb07ebe916b014bc3bccfea6dcf7d3
潘勇 <2472937751@qq.com>
2020-08-25 18:41:30 +0800

    # Leetcode 打卡1题 1.491 递增子序列 半懂

--- src/leetcode/everyday/august/Solution0825.java
+++ src/leetcode/everyday/august/Solution0825.java
@@ -8,23 +8,23 @@ import java.util.List;
  * @create: 2020-08-25 18:19
  * 491. 递增子序列
  * 给定一个整型数组, 你的任务是找到所有该数组的递增子序列，递增子序列的长度至少是2。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 输入: [4, 6, 7, 7]
  * 输出: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
  * 说明:
- *
+ * <p>
  * 给定数组的长度不会超过15。
  * 数组中的整数范围是 [-100,100]。
  * 给定数组中可能包含重复数字，相等的数字应该被视为递增的一种情况。
- *
  **/
 
 
 public class Solution0825 {
     /**
      * 自己写,理解错题意
+     *
      * @param nums
      * @return
      */
@@ -43,15 +43,16 @@ public class Solution0825 {
         }
         return res;
     }
-    List<Integer> temp = new ArrayList<Integer>();
-    List<List<Integer>> ans = new ArrayList<List<Integer>>();
 
-    public List<List<Integer>> findSubsequences(int[] nums) {
+    public static List<Integer> temp = new ArrayList<Integer>();
+    public static List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public static List<List<Integer>> findSubsequences(int[] nums) {
         dfs(0, Integer.MIN_VALUE, nums);
         return ans;
     }
 
-    public void dfs(int cur, int last, int[] nums) {
+    public static void dfs(int cur, int last, int[] nums) {
         if (cur == nums.length) {
             if (temp.size() >= 2) {
                 ans.add(new ArrayList<Integer>(temp));
@@ -67,8 +68,9 @@ public class Solution0825 {
             dfs(cur + 1, last, nums);
         }
     }
+
     public static void main(String[] args) {
-        int[] qus={4,6,7,7};
+        int[] qus = {4, 6, 7, 7};
         System.out.println(findSubsequences(qus));
     }
 }
